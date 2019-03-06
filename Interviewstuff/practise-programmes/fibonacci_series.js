37738431a10c130c02f8d5897be673689d5f4a82
Viswanath Reddy Vangala <vvangala@opentext.com>
2019-03-06 14:00:46 +0530

    MY desktop commits

--- Interviewstuff/practise-programmes/fibonacci_series.js
+++ Interviewstuff/practise-programmes/fibonacci_series.js
@@ -1,38 +1,8 @@
-//fibonacci_series
+// fibonacci_series
 
-var a = b =1 ,c;
-function fibonacci(n){
-      console.log(`${a}`)
-      console.log(b)
-     for(let i=0;i<n;i++){ 
+var a = 1,b=1;
+function fibonacci(){
 	
-		c = a+b
-		if( c >= n){
-			return ;
-	 }
-	 console.log(c)
-		a = b;
-		b = c ;
-    }
+   c= a+b ;
+
 }
-
-fibonacci(50)
-
-
-
-//with recursive 
-
-var f1= 0 ,f2 = 1 ,f3=0;
-console.log(f1 , f2)
-function fibbrec(f1,f2){ 
-   
-	f3 = f1+f2; 
-   if( f3 >= 100){
-   	return;
-   }
-   console.log(f3);
-   f1 = f2
-   f2 = f3
-   return fibb(f1,f2)
-}
-fibb(f1,f2)
