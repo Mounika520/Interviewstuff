446a91fb9de7dc5ab877cd765b5cd3935ea84b36
viswa <v.viswa000@gmail.com>
2019-04-09 14:59:46 +0530

    After ading DOM

---
+++ Interviewstuff/JS_CONCENPTS/FactSet Experience.js
@@ -0,0 +1,45 @@
+//FactSet Experience 
+
+
+var a = 5 ;
+function sum(){
+	console.log(a)
+	a= 10;
+	console.log(a)
+}
+sum()
+
+//output 5,10
+
+
+
+var b = 5 ;
+function sum(){
+	console.log(b)
+	var b= 10;
+	console.log(b)
+}
+sum()
+
+//output undefiend , 10 
+
+
+function sum(){
+	console.log(d)
+	  d = 10 ;
+     var d
+}
+sum()
+
+
+
+
+true  == 'true'  // false
+
+true  === 'true'  // false 
+
+
+Difference btween null vs undefiend vs not-defined
+
+
+Hosting : Varaibles can be accessed before delcartion . Hosting applicable to both Varaibles and function 
---
+++ Interviewstuff/JS_CONCENPTS/thisinjs.js
@@ -0,0 +1,27 @@
+/*  questions on this  */
+
+var obj = {
+   a:5,
+   getvalue : function(){
+   	  console.log(this.a)     //a = 5 
+   }
+
+}
+obj.getvalue()
+
+
+// question
+
+var obj = {
+   a:5,
+   getvalue : function(){
+   	  console.log(this.a)     //a = 5 
+   }
+
+}
+var callme = obj.getvalue
+console.dir(callme)
+callme() // undefiend 
+
+
+callme.call(this,obj)    
